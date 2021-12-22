9c3b1e70361d52004d029c5b4cf6708a019e5e5c
MitchEarnshaw <mitch.earnshaw@gmail.com>
2021-12-22 15:12:42 +1100

    uibuild

--- index.js
+++ index.js
@@ -44,9 +44,9 @@ let persons = [
     }
 ]
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello world!</h1>')
-})
+// app.get('/', (request, response) => {
+//     response.send('<h1>Hello world!</h1>')
+// })
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
