800c44bdd72604f592e1850ee8119b4ffc475f6d
MitchEarnshaw <mitch.earnshaw@gmail.com>
2021-12-22 14:47:14 +1100

    Setup for heroku

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
+
+
 
 morgan.token('postBody', (req) => {
     return JSON.stringify(req.body)
 })
 app.use(express.json())
+
 
 app.use(morgan(':method :url :res[content-length] - :response-time ms :postBody', {
     skip: (req) => req.method !== 'POST'
@@ -98,7 +104,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
